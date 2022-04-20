6722cb077ffd24b75bcb1d73081bfaef36be5c3a
ikhlass-hajjam <ikhlass.hajjam@etud.univ-jfc.fr>
2022-04-21 01:44:23 +0200

    backend

---
+++ horus/src/main/java/com/horushcs/horus/service/PoleService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.horushcs.horus.service;
+
+import com.horushcs.horus.model.Pole;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author hikhl
+ */
+public interface PoleService {
+    public List<Pole> getAllPoles();
+    
+    
+    
+}
